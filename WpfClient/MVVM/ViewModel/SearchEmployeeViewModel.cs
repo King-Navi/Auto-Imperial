ae519f4a86ddcfa977512b34642f8bd9f5454bc9
Mauricio Salas Garcia <zS22013639@estudiantes.uv.mx>
2025-03-17 12:22:02 -0600

    The search employee view has been implemented

--- WpfClient/MVVM/ViewModel/SearchEmployeeViewModel.cs
+++ WpfClient/MVVM/ViewModel/SearchEmployeeViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using Services.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WpfClient.MVVM.Model;
+using WpfClient.Resources.ViewCards;
 using WpfClient.Utilities;
 
 namespace WpfClient.MVVM.ViewModel
@@ -12,6 +14,7 @@ namespace WpfClient.MVVM.ViewModel
     class SearchEmployeeViewModel : Services.Navigation.ViewModel
     {
 
+        public ObservableCollection<EmployeeCardViewModel> EmployeesList { get; set; } = new ObservableCollection<EmployeeCardViewModel>();
 
         private INavigationService navigation;
         public INavigationService Navigation
@@ -26,7 +29,33 @@ namespace WpfClient.MVVM.ViewModel
 
         public SearchEmployeeViewModel(INavigationService navigationService, UserService currentUser)
         {
-            
+            Navigation = navigationService;
+
+            //TODO Omg so much hard code
+            EmployeesList.Add(new EmployeeCardViewModel(navigationService, new Employee
+            {
+                Name = "Maria Jose"
+            }));
+
+            EmployeesList.Add(new EmployeeCardViewModel(navigationService, new Employee
+            {
+                Name = "Carlos Hernández"
+            }));
+            EmployeesList.Add(new EmployeeCardViewModel(navigationService, new Employee
+            {
+                Name = "Carlos Hernández"
+            }));
+            EmployeesList.Add(new EmployeeCardViewModel(navigationService, new Employee
+            {
+                Name = "Carlos Hernández"
+            }));
+            EmployeesList.Add(new EmployeeCardViewModel(navigationService, new Employee
+            {
+                Name = "Carlos Hernández"
+            }));
         }
+
+
+       
     }
 }
