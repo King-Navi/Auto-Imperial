93eaef9078bdc554baae53a3dcab386950bdbb34
unaay20 <junml.kimmich@gmail.com>
2025-03-29 22:45:12 -0600

    Added the command to scaffold

--- AutoImperialDAO/Models/AutoImperialContext.cs
+++ AutoImperialDAO/Models/AutoImperialContext.cs
@@ -41,15 +41,19 @@ public partial class AutoImperialContext : DbContext
 
     public virtual DbSet<Version> Version { get; set; }
 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost;Database=AutoImperial;User Id=devimperial;Password=devimperial;TrustServerCertificate=True;");
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Administrador>(entity =>
         {
-            entity.HasKey(e => e.idAdministrador).HasName("PK__Administ__EBE80EA1AA437AAA");
+            entity.HasKey(e => e.idAdministrador).HasName("PK__Administ__EBE80EA1402C5AD4");
 
-            entity.HasIndex(e => e.numeroEmpleado, "UQ__Administ__49C3142CA0083521").IsUnique();
+            entity.HasIndex(e => e.numeroEmpleado, "UQ__Administ__49C3142CED0DC607").IsUnique();
 
-            entity.HasIndex(e => e.nombreUsuario, "UQ__Administ__A0436BD7A59DB777").IsUnique();
+            entity.HasIndex(e => e.nombreUsuario, "UQ__Administ__A0436BD75B50FC90").IsUnique();
 
             entity.Property(e => e.CURP)
                 .HasMaxLength(50)
@@ -103,9 +107,9 @@ public partial class AutoImperialContext : DbContext
 
         modelBuilder.Entity<Cliente>(entity =>
         {
-            entity.HasKey(e => e.idCliente).HasName("PK__Cliente__885457EE6F718F5A");
+            entity.HasKey(e => e.idCliente).HasName("PK__Cliente__885457EE4A44299C");
 
-            entity.HasIndex(e => e.CURP, "UQ__Cliente__F46C4CBF32CC3BA2").IsUnique();
+            entity.HasIndex(e => e.CURP, "UQ__Cliente__F46C4CBFAE9123DE").IsUnique();
 
             entity.Property(e => e.CURP)
                 .HasMaxLength(50)
@@ -145,7 +149,7 @@ public partial class AutoImperialContext : DbContext
 
         modelBuilder.Entity<CompraProveedor>(entity =>
         {
-            entity.HasKey(e => e.idCompraProveedor).HasName("PK__CompraPr__98B4D02FA6AAAC0F");
+            entity.HasKey(e => e.idCompraProveedor).HasName("PK__CompraPr__98B4D02F78717E84");
 
             entity.Property(e => e.folio)
                 .HasMaxLength(50)
@@ -155,17 +159,17 @@ public partial class AutoImperialContext : DbContext
             entity.HasOne(d => d.idAdministradorNavigation).WithMany(p => p.CompraProveedor)
                 .HasForeignKey(d => d.idAdministrador)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__CompraPro__idAdm__5224328E");
+                .HasConstraintName("FK__CompraPro__idAdm__60A75C0F");
 
             entity.HasOne(d => d.idProveedorNavigation).WithMany(p => p.CompraProveedor)
                 .HasForeignKey(d => d.idProveedor)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__CompraPro__idPro__531856C7");
+                .HasConstraintName("FK__CompraPro__idPro__619B8048");
         });
 
         modelBuilder.Entity<Descuento>(entity =>
         {
-            entity.HasKey(e => e.idDescuento).HasName("PK__Descuent__33F6984681F5B1D2");
+            entity.HasKey(e => e.idDescuento).HasName("PK__Descuent__33F698462F17FC93");
 
             entity.HasMany(d => d.idVehiculo).WithMany(p => p.idDescuento)
                 .UsingEntity<Dictionary<string, object>>(
@@ -173,14 +177,14 @@ public partial class AutoImperialContext : DbContext
                     r => r.HasOne<Vehiculo>().WithMany()
                         .HasForeignKey("idVehiculo")
                         .OnDelete(DeleteBehavior.ClientSetNull)
-                        .HasConstraintName("FK__Descuento__idVeh__51300E55"),
+                        .HasConstraintName("FK__Descuento__idVeh__5FB337D6"),
                     l => l.HasOne<Descuento>().WithMany()
                         .HasForeignKey("idDescuento")
                         .OnDelete(DeleteBehavior.ClientSetNull)
-                        .HasConstraintName("FK__Descuento__idDes__503BEA1C"),
+                        .HasConstraintName("FK__Descuento__idDes__5EBF139D"),
                     j =>
                     {
-                        j.HasKey("idDescuento", "idVehiculo").HasName("PK__Descuent__27701AD114B79F40");
+                        j.HasKey("idDescuento", "idVehiculo").HasName("PK__Descuent__27701AD10F791F56");
                         j.HasIndex(new[] { "idDescuento" }, "idx_idDescuentoVehiculo");
                         j.HasIndex(new[] { "idVehiculo" }, "idx_idVehiculoDescuentoVehiculo");
                     });
@@ -188,19 +192,19 @@ public partial class AutoImperialContext : DbContext
 
         modelBuilder.Entity<Fotos>(entity =>
         {
-            entity.HasKey(e => e.idFotos).HasName("PK__Fotos__6E121FAC88311BA0");
+            entity.HasKey(e => e.idFotos).HasName("PK__Fotos__6E121FACEB8DCFAF");
 
             entity.HasIndex(e => e.idVehiculo, "idx_idVehiculoFotos");
 
             entity.HasOne(d => d.idVehiculoNavigation).WithMany(p => p.Fotos)
                 .HasForeignKey(d => d.idVehiculo)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__Fotos__idVehicul__4D5F7D71");
+                .HasConstraintName("FK__Fotos__idVehicul__5BE2A6F2");
         });
 
         modelBuilder.Entity<Marca>(entity =>
         {
-            entity.HasKey(e => e.idMarca).HasName("PK__Marca__7033181281574284");
+            entity.HasKey(e => e.idMarca).HasName("PK__Marca__703318121D6171D2");
 
             entity.Property(e => e.nombre)
                 .HasMaxLength(50)
@@ -209,7 +213,7 @@ public partial class AutoImperialContext : DbContext
 
         modelBuilder.Entity<Modelo>(entity =>
         {
-            entity.HasKey(e => e.idModelo).HasName("PK__Modelo__13A52CD1E59BFCED");
+            entity.HasKey(e => e.idModelo).HasName("PK__Modelo__13A52CD198499B19");
 
             entity.Property(e => e.nombre)
                 .HasMaxLength(50)
@@ -218,12 +222,12 @@ public partial class AutoImperialContext : DbContext
             entity.HasOne(d => d.idMarcaNavigation).WithMany(p => p.Modelo)
                 .HasForeignKey(d => d.idMarca)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__Modelo__idMarca__4E53A1AA");
+                .HasConstraintName("FK__Modelo__idMarca__5CD6CB2B");
         });
 
         modelBuilder.Entity<Proveedor>(entity =>
         {
-            entity.HasKey(e => e.idProveedor).HasName("PK__Proveedo__A3FA8E6BCD79E162");
+            entity.HasKey(e => e.idProveedor).HasName("PK__Proveedo__A3FA8E6B007B2A97");
 
             entity.Property(e => e.calle)
                 .HasMaxLength(50)
@@ -253,7 +257,7 @@ public partial class AutoImperialContext : DbContext
 
         modelBuilder.Entity<Reserva>(entity =>
         {
-            entity.HasKey(e => e.idReserva).HasName("PK__Reserva__94D104C84C564BEF");
+            entity.HasKey(e => e.idReserva).HasName("PK__Reserva__94D104C8E6E81C89");
 
             entity.HasIndex(e => e.idCliente, "idx_idCliente");
 
@@ -274,22 +278,22 @@ public partial class AutoImperialContext : DbContext
             entity.HasOne(d => d.idClienteNavigation).WithMany(p => p.Reserva)
                 .HasForeignKey(d => d.idCliente)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__Reserva__idClien__47A6A41B");
+                .HasConstraintName("FK__Reserva__idClien__5629CD9C");
 
             entity.HasOne(d => d.idVendedorNavigation).WithMany(p => p.Reserva)
                 .HasForeignKey(d => d.idVendedor)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__Reserva__idVende__46B27FE2");
+                .HasConstraintName("FK__Reserva__idVende__5535A963");
 
             entity.HasOne(d => d.idVersionNavigation).WithMany(p => p.Reserva)
                 .HasForeignKey(d => d.idVersion)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__Reserva__idVersi__489AC854");
+                .HasConstraintName("FK__Reserva__idVersi__571DF1D5");
         });
 
         modelBuilder.Entity<Vehiculo>(entity =>
         {
-            entity.HasKey(e => e.idVehiculo).HasName("PK__Vehiculo__48682970000F4F08");
+            entity.HasKey(e => e.idVehiculo).HasName("PK__Vehiculo__48682970E4D2D424");
 
             entity.HasIndex(e => e.idCompraProveedor, "idx_idCompraProveedor");
 
@@ -319,21 +323,21 @@ public partial class AutoImperialContext : DbContext
             entity.HasOne(d => d.idCompraProveedorNavigation).WithMany(p => p.Vehiculo)
                 .HasForeignKey(d => d.idCompraProveedor)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__Vehiculo__idComp__4B7734FF");
+                .HasConstraintName("FK__Vehiculo__idComp__59FA5E80");
 
             entity.HasOne(d => d.idVersionNavigation).WithMany(p => p.Vehiculo)
                 .HasForeignKey(d => d.idVersion)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__Vehiculo__idVers__4C6B5938");
+                .HasConstraintName("FK__Vehiculo__idVers__5AEE82B9");
         });
 
         modelBuilder.Entity<Vendedor>(entity =>
         {
-            entity.HasKey(e => e.idVendedor).HasName("PK__Vendedor__A6693F9335E76A3B");
+            entity.HasKey(e => e.idVendedor).HasName("PK__Vendedor__A6693F935694E687");
 
-            entity.HasIndex(e => e.numeroEmpleado, "UQ__Vendedor__49C3142C2F28F930").IsUnique();
+            entity.HasIndex(e => e.numeroEmpleado, "UQ__Vendedor__49C3142C1FF4770C").IsUnique();
 
-            entity.HasIndex(e => e.nombreUsuario, "UQ__Vendedor__A0436BD714DDD595").IsUnique();
+            entity.HasIndex(e => e.nombreUsuario, "UQ__Vendedor__A0436BD723D596C5").IsUnique();
 
             entity.Property(e => e.CURP)
                 .HasMaxLength(50)
@@ -387,7 +391,7 @@ public partial class AutoImperialContext : DbContext
 
         modelBuilder.Entity<Venta>(entity =>
         {
-            entity.HasKey(e => e.idVenta).HasName("PK__Venta__077D56147663407C");
+            entity.HasKey(e => e.idVenta).HasName("PK__Venta__077D5614A44EF779");
 
             entity.Property(e => e.formaPago)
                 .HasMaxLength(40)
@@ -400,17 +404,17 @@ public partial class AutoImperialContext : DbContext
             entity.HasOne(d => d.idReservaNavigation).WithMany(p => p.Venta)
                 .HasForeignKey(d => d.idReserva)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__Venta__idReserva__498EEC8D");
+                .HasConstraintName("FK__Venta__idReserva__5812160E");
 
             entity.HasOne(d => d.idVehiculoNavigation).WithMany(p => p.Venta)
                 .HasForeignKey(d => d.idVehiculo)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__Venta__idVehicul__4A8310C6");
+                .HasConstraintName("FK__Venta__idVehicul__59063A47");
         });
 
         modelBuilder.Entity<Version>(entity =>
         {
-            entity.HasKey(e => e.idVersion).HasName("PK__Version__BBD5F8B29319AEDF");
+            entity.HasKey(e => e.idVersion).HasName("PK__Version__BBD5F8B26C508228");
 
             entity.Property(e => e.motor)
                 .HasMaxLength(50)
@@ -425,7 +429,7 @@ public partial class AutoImperialContext : DbContext
             entity.HasOne(d => d.idModeloNavigation).WithMany(p => p.Version)
                 .HasForeignKey(d => d.idModelo)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__Version__idModel__4F47C5E3");
+                .HasConstraintName("FK__Version__idModel__5DCAEF64");
         });
 
         OnModelCreatingPartial(modelBuilder);
